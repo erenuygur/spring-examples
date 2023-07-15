0fc1a95d7f8698b2e7b05ce234ed4117e2fa381b
Eren <ernuygur@gmail.com>
2023-07-15 03:32:52 +0300

    add 1-helloworld springboot project

---
+++ helloworld/src/main/java/com/erenuygur/helloworld/HelloApi.java
@@ -0,0 +1,15 @@
+package com.erenuygur.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class HelloApi {
+
+    @GetMapping
+    public String hello() {
+        return "Hello, World";
+    }
+}
